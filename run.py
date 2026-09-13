9b68d5833acc8381138618f240da410c644c1dd4
Matt Birum <birum18@gmail.com>
2026-09-13 09:03:13 -0400

    wire directly in

--- run.py
+++ run.py
@@ -40,22 +40,22 @@ def on_press(key):
         if direction < 0:
             # left
             print("left")
-            GPIO.output(left_pin, 1)
+            GPIO.output(left_pin, GPIO.HIGH)
             time.sleep(1)
-            GPIO.output(left_pin, 0)
+            GPIO.output(left_pin, GPIO.LOW)
             print("--")
         elif direction > 0:
             # right
             print("right")
-            GPIO.output(right_pin, 1)
+            GPIO.output(right_pin, GPIO.HIGH)
             time.sleep(1)
-            GPIO.output(right_pin, 0)
+            GPIO.output(right_pin, GPIO.LOW)
             print("--")
     elif axis == "y":
         if direction > 0:
             # up
             print("up")
-            GPIO.output(up_pin, 1)
+            GPIO.output(up_pin, GPIO.HIGH)
             # GPIO.output(up_pin, 1)
             # time.sleep(2)
             # GPIO.output(up_pin, 0)
@@ -63,10 +63,10 @@ def on_press(key):
         elif direction < 0:
             # down
             print("down")
-            GPIO.output(up_pin, 0)
-            GPIO.output(down_pin, 1)
+            GPIO.output(up_pin, GPIO.LOW)
+            GPIO.output(down_pin, GPIO.HIGH)
             time.sleep(1)
-            GPIO.output(down_pin, 0)
+            GPIO.output(down_pin, GPIO.LOW)
             print("--")
 
 def main():
